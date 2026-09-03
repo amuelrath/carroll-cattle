b6a023b74e7f23bd59f724522a31aaddad105081
Austin Muelrath <austinmuelrath@gmail.com>
2026-09-02 21:18:10 -0700

    add icons and example table data

--- src/shared/resources/strings/processes.ts
+++ src/shared/resources/strings/processes.ts
@@ -1,25 +1,56 @@
 import { TInsertProcess } from '@shared/db/schema'
 import { TProcess } from '@shared/schemas/process'
+import {
+  BabyIcon,
+  BirdIcon,
+  LucideIcon,
+  ShelvingUnitIcon,
+  SyringeIcon,
+  TestTubeIcon
+} from 'lucide-react'
+import {
+  breedRecipExamples,
+  calveRecipExamples,
+  penguinExamples,
+  pregCheckExamples,
+  transOvaInvExamples
+} from '../schemas/process-examples'
 
-export const processesCopy: Record<TProcess, Omit<TInsertProcess, 'id'>> = {
+export const PROCESS_COPY: Record<
+  TProcess,
+  Omit<TInsertProcess, 'id'> & {
+    Icon: LucideIcon
+    exampleData: Record<string, any>[]
+  }
+> = {
   penguins: {
     name: 'Penguins',
-    desc: 'Palmer MF Penguins!!!'
+    desc: 'Palmer MF Penguins!!!',
+    Icon: BirdIcon,
+    exampleData: penguinExamples
   },
   breedRecip: {
     name: 'Breed Recipients',
-    desc: 'Update recipient profiles in CattleMax.'
+    desc: 'Update recipient profiles in CattleMax.',
+    Icon: SyringeIcon,
+    exampleData: breedRecipExamples
   },
   calveRecip: {
     name: 'Calve Recipients',
-    desc: 'Update recipient profiles and create calf profiles in CattleMax.'
+    desc: 'Update recipient profiles and create calf profiles in CattleMax.',
+    Icon: BabyIcon,
+    exampleData: calveRecipExamples
   },
   pregCheck: {
     name: 'Pregnancy Checks',
-    desc: 'Update recipient profiles in CattleMax.'
+    desc: 'Update recipient profiles in CattleMax.',
+    Icon: TestTubeIcon,
+    exampleData: pregCheckExamples
   },
   transOvaInventory: {
     name: 'TransOva Inventory',
-    desc: 'Add new embryo shipment to inventory in CattleMax.'
+    desc: 'Add new embryo shipment to inventory in CattleMax.',
+    Icon: ShelvingUnitIcon,
+    exampleData: transOvaInvExamples
   }
 } as const
