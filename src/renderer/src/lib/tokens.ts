5fa20cc0816e047676320e591701c2c2ec8a5318
Austin Muelrath <austinmuelrath@gmail.com>
2026-09-02 21:15:00 -0700

    add iconSize tokens

---
+++ src/renderer/src/lib/tokens.ts
@@ -0,0 +1,18 @@
+export const iconSizes = {
+  small: {
+    size: 12,
+    strokeWidth: 1
+  },
+  medium: {
+    size: 16,
+    strokeWidth: 1
+  },
+  large: {
+    size: 20,
+    strokeWidth: 1
+  },
+  xlarge: {
+    size: 32,
+    strokeWidth: 1
+  }
+} as const
