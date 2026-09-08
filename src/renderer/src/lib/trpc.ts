9e6704eb645c36b2c041d32bba0f17a2aa663bfd
Austin Muelrath, MS <151596558+amuelrath@users.noreply.github.com>
2026-09-07 18:39:39 -0700

    refactor trpc logging/error handling (#8)

--- src/renderer/src/lib/trpc.ts
+++ src/renderer/src/lib/trpc.ts
@@ -1,19 +1,18 @@
-import { createTRPCProxyClient, TRPCClientError, TRPCLink } from '@trpc/client'
 import type { AppRouter } from '@main/trpc'
-import { ipcLink } from 'trpc-electron/renderer'
-import { navigate } from './nav-ref'
-import { toast } from '../components/ui/toast'
-import { observable } from '@trpc/server/observable'
 import { errors } from '@shared/resources/strings'
+import { createTRPCProxyClient, TRPCLink } from '@trpc/client'
+import { observable } from '@trpc/server/observable'
+import log from 'electron-log/renderer'
+import { ipcLink } from 'trpc-electron/renderer'
+import { toast } from '../components/ui/toast'
+import { navigate } from './nav-ref'
 
 const responseLink: TRPCLink<AppRouter> = () => {
-  // everything in this section happens once during app initialization
-  // useful for cache storage - docs
   return ({ next, op }) => {
     return observable((observer) => {
       return next(op).subscribe({
         next(value) {
-          // middleware here
+          // eslint-disable-next-line
           const res = value.result.data as any
           if (res?.redirect) navigate(res.redirect)
           if (res?.toast) {
@@ -22,17 +21,16 @@ const responseLink: TRPCLink<AppRouter> = () => {
               description: res.toast.message
             })
           }
-          if (res?.status === 'error') {
-            observer.error(new TRPCClientError(res.message))
-            return
-          }
           observer.next(value)
         },
         error(err) {
           toast.add({
-            type: 'error',
-            description: errors.unknown
+            type: err.data?.toast?.type ?? 'error',
+            description: err.data?.toast?.message ?? errors.unknown
           })
+          if (err.data?.toast) {
+            log.error(err.message, err.data?.causeMessage ?? err.cause)
+          }
           observer.error(err)
         },
         complete() {
